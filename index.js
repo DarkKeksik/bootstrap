f2ad4390ce4b611f74ec3fab5067faa3273e36c7
DarkKeksik <frilans.er@mail.ru>
2019-12-08 23:22:56 +1000

    Added db.rar and the ability to display the news page without restarting the server

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 const express = require("express");
 const app = express();
 const http = require("http").createServer(app).listen(8080);
-const mongoDB_news = require("./template/serverJS/mongoConfig.js");
+const mongoConfig = require("./template/serverJS/mongoConfig.js");
 
 app.set("view engine", "ejs");
 app.use("/bootstrap", express.static(`${__dirname}/node_modules/bootstrap/dist`));
@@ -10,9 +10,25 @@ app.use("/jquery", express.static(`${__dirname}/node_modules/jquery/dist`));
 app.use("/favicon", express.static(`${__dirname}/node_modules/font-awesome`));
 app.use("/static", express.static(`${__dirname}/template`));
 
-app.get("/", (req, res) => {
-    res.render(`${__dirname}/template/pages/main`, {
-        allNews: mongoDB_news
+mongoConfig.connectBD().then((data)=>{
+    let collection = data.collection("news").find({title: {$exists: true}}, {_id: 0});
+    mongoConfig.collectionInArr(collection).then((data)=>{
+        console.log("test");
+        console.log(data);
     });
 });
 
+app.get("/", (req, res) => {
+    mongoConfig.connectBD().then((data) => {        
+        // Выбираем коллекцию из базы
+        let collection = data.collection("news").find({title: {$exists: true}}, {_id: 0});
+        
+        // Составляем массив и посылаем клиенту
+        mongoConfig.collectionInArr(collection).then((data) => {
+            res.render(`${__dirname}/template/pages/main`, {
+                allNews: data
+            });
+        });
+        
+    });
+}); 
--- template/serverJS/mongoConfig.js
+++ template/serverJS/mongoConfig.js
@@ -8,19 +8,48 @@ let client = new mongoClient(url);
 let dbName = "catblog";
 
 // Подключение к БД
-client.connect(err => {
-    assert.equal(null, err);
-    let db = client.db(dbName);
-    let collection = db.collection("news").find({title: {$exists: true}}, {_id: 0});
-    
-    let allNews = [];
-    collection.each((err, item)=>{
-        if (item !== null) {
-            allNews.push(item);
-        };
+let connectDB = () => {
+    return new Promise((resolve, reject) => {
+        client.connect(err => {
+            return resolve(client.db(dbName));
+        });
     });
-    
-    // Импортируем полученную db в главный модуль
-    module.exports.allNews = allNews;
-    client.close();
-});
+};
+
+// Составляем массив из коллекции MongoDB
+collectionInArr = (collection) => {
+    return new Promise((resolve, reject) => {
+        let allNews = [];
+        
+        collection.forEach((item) => {
+            allNews.push(item);
+        }, (end) => {
+            if (end === null) { resolve(allNews); }
+            else { console.log(`Error: ${end}`); }
+        });
+        
+    });
+};
+
+module.exports.connectBD = connectDB;
+module.exports.collectionInArr = collectionInArr;
+
+
+
+
+//client.connect(err => {
+//    assert.equal(null, err);
+//    let db = client.db(dbName);
+//    let collection = db.collection("news").find({title: {$exists: true}}, {_id: 0});
+//    
+//    let allNews = [];
+//    collection.forEach((item) => {
+//        allNews.push(item);
+//    }, ()=> {
+//        console.log("Good");
+//    });
+//    
+//    // Импортируем полученную db в главный модуль
+//    module.exports.allNews = allNews;
+//    client.close();
+//});
